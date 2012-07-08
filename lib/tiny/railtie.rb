09d73f5da84625d6bfbd0cdd862df71c18014b06
maca <mail@makarius.me>
2012-07-08 16:38:06 -0500

    removed railtie

--- lib/tiny/railtie.rb
+++
@@ -1,9 +0,0 @@
-require 'tiny'
-module Tiny
-  class Railtie < Rails::Railtie
-    initializer "tiny.helper" do
-      puts 'Actionview'
-      ActionView::Base.send :include, Helper
-    end
-  end
-end
