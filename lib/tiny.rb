e5da60791d0f2dc07d733e0c7e7b039a65c2b493
maca <mail@makarius.me>
2012-07-30 02:08:47 -0500

    tiny refactor

--- lib/tiny.rb
+++ lib/tiny.rb
@@ -167,21 +167,17 @@ module Tiny
     end
     
     def render scope, &block
-      return void_tag if tag_void?
-
       content = scope.tiny_capture(self, &block) if block_given?
       scope.tiny_concat content_tag(content)
     end
 
-    def tag_void?
+    def void_tag?
       HTMLTags.void_tags.include? tag_name
     end
 
-    def void_tag
-      "<#{tag_name}#{tag_attributes} />"
-    end
-
     def content_tag content
+      return "<#{tag_name}#{tag_attributes} />" if void_tag?
+
       if content
         content.gsub!(/^(?!\s*$)/, "  ")
         content.gsub!(/\A(?!$)|(?<!^|\n)\z/, "\n") 
